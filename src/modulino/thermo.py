62dbb1ecaf87e5b3ef8a1ea0a42b8aeefe6a2e26
Sebastian Romero <s.romero@arduino.cc>
2024-07-03 23:20:10 +0200

    Rename bus variable

--- src/modulino/thermo.py
+++ src/modulino/thermo.py
@@ -10,7 +10,7 @@ class ModulinoThermo():
     DEFAULT_ADDRESS = const(0x44)
 
     def __init__(self, i2c_bus, address: int = DEFAULT_ADDRESS) -> None:
-        self._i2c = i2c_bus
+        self._i2c_bus = i2c_bus
         self._address = address
         self._status_bit = None
 
@@ -19,10 +19,10 @@ class ModulinoThermo():
         """
         Return Temperature and Relative Humidity or None if the data is stalled
         """
-        self._i2c.writeto(self._address, bytes([0x00]))
+        self._i2c_bus.writeto(self._address, bytes([0x00]))
         sleep(0.1)  # Time to wake up the sensor
         data = bytearray(4)
-        self._i2c.readfrom_into(self._address, data)
+        self._i2c_bus.readfrom_into(self._address, data)
 
         # The Status bit will have a value of 1 when the data is stalled
         self._status_bit = data[0] & 0x40
