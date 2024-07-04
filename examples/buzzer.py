c5e6f631bc935ac8a97f791cf8016d8be7d28355
Sebastian Romero <s.romero@arduino.cc>
2024-07-04 08:43:55 +0200

    Refactor buzzer

--- examples/buzzer.py
+++ examples/buzzer.py
@@ -1,24 +1,22 @@
 from modulino import ModulinoBuzzer
-from machine import SoftI2C, I2C, Pin
-from time import sleep
 
-bus = I2C(0, sda=Pin(43), scl=Pin(44))
-buzzer = ModulinoBuzzer(bus)
+buzzer = ModulinoBuzzer()
 buzzer.begin()
 
+melody = [
+    (ModulinoBuzzer.PITCHES["NOTE_E5"], 125),
+    (ModulinoBuzzer.PITCHES["NOTE_E5"], 125),
+    (ModulinoBuzzer.PITCHES["REST"], 125),
+    (ModulinoBuzzer.PITCHES["NOTE_E5"], 125),
+    (ModulinoBuzzer.PITCHES["REST"], 125),
+    (ModulinoBuzzer.PITCHES["NOTE_C5"], 125),
+    (ModulinoBuzzer.PITCHES["NOTE_E5"], 125),
+    (ModulinoBuzzer.PITCHES["REST"], 125),
+    (ModulinoBuzzer.PITCHES["NOTE_G5"], 125),
+    (ModulinoBuzzer.PITCHES["REST"], 375),
+    (ModulinoBuzzer.PITCHES["NOTE_G4"], 250)
+]
+
 # Super Mario Bros theme
-buzzer.tone(659, 125)
-sleep(0.2)
-buzzer.tone(659, 125)
-sleep(0.2)
-buzzer.tone(659, 125)
-sleep(0.2)
-buzzer.tone(523, 125)
-sleep(0.2)
-buzzer.tone(659, 125)
-sleep(0.2)
-buzzer.tone(784, 125)
-sleep(0.2)
-buzzer.tone(392, 125)
-sleep(0.2)
-buzzer.no_tone()
+for note, duration in melody:
+    buzzer.tone(note, duration)
