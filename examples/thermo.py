a2cb12da224547336798193b515aea9ffa90c98e
Sebastian Romero <s.romero@arduino.cc>
2024-07-08 15:51:16 +0200

    Add check to thermo example

--- examples/thermo.py
+++ examples/thermo.py
@@ -1,7 +1,12 @@
 from modulino import ModulinoThermo
 from time import sleep
+from sys import exit
 
 thermo_module = ModulinoThermo()
+
+if not thermo_module:
+    print("🤷 No thermo modulino found")    
+    exit()
 
 while True:
     print(f"🌡️ Temperature: {thermo_module.temperature:.1f} °C")
