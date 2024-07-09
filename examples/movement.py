07eff79ef930d79484cbdb2f0b6ce5b2455099e9
Sebastian Romero <s.romero@arduino.cc>
2024-07-09 13:42:27 +0200

    Allow autodiscovery of movement module

--- examples/movement.py
+++ examples/movement.py
@@ -1,9 +1,7 @@
 from modulino import ModulinoMovement
-from machine import I2C, Pin
 from time import sleep_ms
 
-bus = I2C(0, sda=Pin(43), scl=Pin(44))
-movement = ModulinoMovement(bus)
+movement = ModulinoMovement()
 
 while True:
     print("🏃 Accelerometer: x:{:>8.3f} y:{:>8.3f} z:{:>8.3f}".format(*movement.accelerometer))
--- src/modulino/movement.py
+++ src/modulino/movement.py
@@ -1,18 +1,18 @@
+from .modulino import Modulino
 from lsm6dsox import LSM6DSOX
-from micropython import const
 
-class ModulinoMovement():
-    DEFAULT_ADDRESS = const(0x6A)
+class ModulinoMovement(Modulino):
+    default_addresses = [0x6A, 0x6B]
+    convert_default_addresses = False
 
-    def __init__(self, i2c_bus, address: int = DEFAULT_ADDRESS) -> None:
-        self._i2c_bus = i2c_bus
-        self._address = address
-        self._sensor = LSM6DSOX(i2c_bus)
+    def __init__(self, i2c_bus=None, address: int | None = None) -> None:
+        super().__init__(i2c_bus, address, "MOVEMENT")
+        self.sensor = LSM6DSOX(self.i2c_bus, address=self.address)
 
     @property
     def accelerometer(self) -> tuple:
-        return self._sensor.accel()
+        return self.sensor.accel()
     
     @property
     def gyro(self) -> tuple:
-        return self._sensor.gyro()
+        return self.sensor.gyro()
