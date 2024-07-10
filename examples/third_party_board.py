ca31016147d7a051abee6409dd6f30256a5cd071
Sebastian Romero <s.romero@arduino.cc>
2024-07-10 17:48:13 +0200

    Add more info to 3rd party board example

--- examples/third_party_board.py
+++ examples/third_party_board.py
@@ -1,7 +1,9 @@
 from modulino import Modulino
-from machine import I2C
+from machine import I2C, Pin
 
+# The modulinos use a frequency of 100kHz by default.
 bus = I2C(0, freq=100000)
+# bus = I2C(0, scl=Pin(18), sda=Pin(19), freq=100000) # If you need to specify the pins
 
 # In case the board was reset in the mean time some modulinos might
 # be stuck in a pending operation. To get them unstuck we need to reset the bus.
