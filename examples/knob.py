1177a8a9fde9e9615277cc5e6ca31c2e1b10f74e
Sebastian Romero <s.romero@arduino.cc>
2024-07-10 15:18:34 +0200

    Refactor knob

--- examples/knob.py
+++ examples/knob.py
@@ -2,14 +2,14 @@ from modulino import ModulinoKnob
 from time import sleep
 
 knob = ModulinoKnob()
-knob.begin()
+knob.value = 128 # (Optional) Set an initial value
 previous_knob_value = None
 
 while True:
-    new_knob_value = knob.get()
+    new_knob_value = knob.value
     if new_knob_value != previous_knob_value:
         previous_knob_value = new_knob_value
         print(f"🎛️ Value: {new_knob_value}")
     
-    print(f"🔘 Pressed: {knob.pressed}")
+    #print(f"🔘 Pressed: {knob.pressed}")
     sleep(0.1)
--- src/modulino/knob.py
+++ src/modulino/knob.py
@@ -6,34 +6,39 @@ class ModulinoKnob(Modulino):
   
   def __init__(self, i2c_bus=None, address=None):
     super().__init__(i2c_bus, address, "ENCODER")
-    self.data = bytearray(3)
+    self._data = bytearray(3)
     self._pressed = False
-    self.value = 0
-    self.bug_on_set = True
-
-  def begin(self):
-    val = self.get()
-    self.set(100)
-    if (self.get() != 100):
-      self.bug_on_set = True
-      self.set(-val)
+    self._value = 0
+    # Flag to detect bug in the set command that would make
+    # the encoder value become negative after setting it to x with x != 0 
+    self._set_bug_detected = False
+    original_value = self.value
+    self.value = 100    
+    
+    # If the value is not 100, then the set command has a bug
+    if (self.value != 100):
+      self._set_bug_detected = True
+      self.value = -original_value
     else:
-      self.set(val)
-    
-  def get(self):
-    self.data = self.read(3)
-    self._pressed = self.data[2] != 0
-    self.value = int.from_bytes(self.data[0:2], 'little', True)
-    
-    if self.value >= 32768:
-      self.value = self.value - 65536
-    return self.value
+      self.value = original_value
 
-  def set(self, value):
-    if (self.bug_on_set):
-      value = -value
+  @property
+  def value(self):
+    self._data = self.read(3)
+    self._pressed = self._data[2] != 0
+    self._value = int.from_bytes(self._data[0:2], 'little', True)
+
+    # Convert to signed int (16 bits)
+    if self._value >= 32768:
+      self._value = self._value - 65536
+    return self._value
+
+  @value.setter
+  def value(self, new_value):
+    if (self._set_bug_detected):
+      new_value = -new_value
     buf = bytearray(4)
-    buf[0:2] = value.to_bytes(2, 'little')
+    buf[0:2] = new_value.to_bytes(2, 'little')
     self.write(buf)
 
   @property
