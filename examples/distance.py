7f98063d69c333a2a9298fe31a1049a027d5be7f
Sebastian Romero <s.romero@arduino.cc>
2024-07-10 17:16:58 +0200

    Add support for distance modulino

--- examples/distance.py
+++ examples/distance.py
@@ -1,12 +1,8 @@
 from modulino import ModulinoDistance
-from machine import I2C, Pin
 from time import sleep_ms
 
-bus = I2C(0, sda=Pin(43), scl=Pin(44))
-distance = ModulinoDistance(bus)
-distance.begin()
+distance = ModulinoDistance()
 
 while True:
     print(f"📏 Distance: {distance.distance} cm")
-    print("")
-    sleep_ms(500)
+    sleep_ms(50)
