c6d1cac6c90138c1a6801dbc0284e3cbc8f2450e
Sebastian Romero <s.romero@arduino.cc>
2024-07-11 23:38:59 +0200

    Improve reliability

--- src/modulino/distance.py
+++ src/modulino/distance.py
@@ -7,12 +7,22 @@ class ModulinoDistance(Modulino):
 
     def __init__(self, i2c_bus = None, address: int = DEFAULT_ADDRESS) -> None:
         super().__init__(i2c_bus, address, "DISTANCE")
-        self.sensor = VL53L4CD(self.i2c_bus, self.address)        
+        self.sensor = VL53L4CD(self.i2c_bus, self.address) 
+        self.sensor.timing_budget = 20     
+        self.sensor.inter_measurement = 0
         self.sensor.start_ranging()
 
     @property
-    def distance(self):
+    def _distance_raw(self):
         while not self.sensor.data_ready:
             pass
         self.sensor.clear_interrupt()
         return self.sensor.distance
+
+    @property
+    def distance(self):
+        while True:
+            raw_distance = self._distance_raw
+            # Filter out invalid readings
+            if raw_distance > 0:
+                return raw_distance
