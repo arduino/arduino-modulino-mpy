6ac881d58183397eab32fed5cdbf1289575de90f
Sebastian Romero <s.romero@arduino.cc>
2024-07-18 09:23:56 +0200

    Use callback parameter

--- examples/knob_buzzer.py
+++ examples/knob_buzzer.py
@@ -19,13 +19,13 @@ notes = sorted(list(filter(lambda note: note >= 400 and note <= 2000, ModulinoBu
 knob.range = (0, len(notes) - 1)
 knob.on_press = lambda : buzzer.no_tone()
 
-def on_knob_rotate_clockwise(_):
-    frequency = notes[knob.value]
+def on_knob_rotate_clockwise(value):
+    frequency = notes[value]
     print(f"🎵 Frequency: {frequency} Hz")
     buzzer.tone(frequency)
 
-def on_knob_rotate_counter_clockwise(_):
-    frequency = notes[knob.value]
+def on_knob_rotate_counter_clockwise(value):
+    frequency = notes[value]
     print(f"🎵 Frequency: {frequency} Hz")
     buzzer.tone(frequency)
 
