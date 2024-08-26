e90f2a115d63ade75762073841686e4b00b0a562
Sebastian Romero <s.romero@arduino.cc>
2024-08-26 16:32:23 +0200

    Use discovery mechanism from super class

--- src/modulino/distance.py
+++ src/modulino/distance.py
@@ -1,11 +1,11 @@
-from micropython import const
 from .modulino import Modulino
 from .lib.vl53l4cd import VL53L4CD
 
 class ModulinoDistance(Modulino):
-    DEFAULT_ADDRESS = const(0x29)
+    default_addresses = [0x29]
+    convert_default_addresses = False
 
-    def __init__(self, i2c_bus = None, address: int = DEFAULT_ADDRESS) -> None:
+    def __init__(self, i2c_bus = None, address: int | None = None) -> None:
         super().__init__(i2c_bus, address, "DISTANCE")
         self.sensor = VL53L4CD(self.i2c_bus, self.address) 
         self.sensor.timing_budget = 20     
