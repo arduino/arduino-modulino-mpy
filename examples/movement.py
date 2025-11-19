36548340daad70da06309743373e8f7cc302d66a
Sebastian Romero <s.romero@arduino.cc>
2025-11-19 16:40:10 +0100

    Rename the movement properties

--- examples/movement.py
+++ examples/movement.py
@@ -11,10 +11,11 @@ from time import sleep_ms
 movement = ModulinoMovement()
 
 while True:
-    acc = movement.accelerometer
-    gyro = movement.gyro
+    acc = movement.acceleration
+    gyro = movement.angular_velocity
     
-    print(f"🏃 Accelerometer: x:{acc.x:>8.3f} y:{acc.y:>8.3f} z:{acc.z:>8.3f}")
-    print(f"🌐 Gyroscope:     x:{gyro.x:>8.3f} y:{gyro.y:>8.3f} z:{gyro.z:>8.3f}")
+    print(f"🏃 Acceleration: x:{acc.x:>8.3f} y:{acc.y:>8.3f} z:{acc.z:>8.3f}")
+    print(f"💪 Acceleration Magnitude: {movement.acceleration_magnitude:>8.3f} g")
+    print(f"🌐 Angular Velocity: x:{gyro.x:>8.3f} y:{gyro.y:>8.3f} z:{gyro.z:>8.3f}")
     print("")
     sleep_ms(100)
